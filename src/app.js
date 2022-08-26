6cfbbea551f3fd2116a48963f898acb2990723a1
DnZ19 <dennis@centerofagile.nl>
2022-08-26 07:37:10 +0200

    last update

--- src/app.js
+++ src/app.js
@@ -1,42 +1,76 @@
 import axios from "axios";
+import 'dotenv/config';
 
-const APP_ID = "a172f123";
-const APP_KEY = "0561d217ec8b90c32ca3264b626f0f63";
+
+require('dotenv').config();
+//console.log(process.env.API_ID);
+
+const APP_ID = process.env.API_ID;
+const APP_KEY = process.env.API_KEY;
 
 let fieldValue = "";
+
 
 //function to get data via API with search param (needs param via getRecipes...)
 const getRecipes = async ( searchString ) => {
     try {
+// set to 1 for API limit purpose
+        for (let i = 0; i < 1; i++) {
 
-        const response = await axios.get(`https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-        console.log(response.data.hits)
+            const response = await axios.get(`https://api.edamam.com/search?q=${searchString}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+            // console.log(response.data.hits)
 
-        const recipeDisplay = document.getElementById("recipe-idea");
-        recipeDisplay.textContent = response.data.hits[0].recipe.label;
+            //destructuring
+            const {label} = response.data.hits[i].recipe;
+            const {image} = response.data.hits[i].recipe;
 
-        const recipeImage = document.createElement("img")
-        recipeImage.setAttribute("src", `response.data.hits[0].recipe.image`);
+            // if statement if --> response then....
+            // if (response)   {
+            //
+            // } else  {
+            //
+            // }
 
-    }
+            function listOfRecipes ()   {
 
-    catch ( e )   {
+                //Label
+                const listItem = document.createElement("LI");
+                listItem.classList.add("recipeLabel");
+                const labelItem = document.createTextNode(label);
+                listItem.appendChild(labelItem);
+                document.getElementById("recipe-items").appendChild(listItem);
+
+                //image
+                const imageItems = document.createElement("IMG");
+                imageItems.classList.add("recipeImage");
+                imageItems.setAttribute('src', image);
+                //imageItems.appendChild(imageItem);
+                document.getElementById("image-items").appendChild(imageItems);
+
+
+
+            }
+            listOfRecipes();
+
+
+
+        }
+    } catch ( e )   {
         console.error( e );
     }
-
 }
 
-//searchfield to fill the param for the API
+
+
+//search field to fill the param for the API
+
 const searchField = document.getElementById("search");
 searchField.addEventListener("keyup", handleChange)
 
 function handleChange(e){
-
     fieldValue = e.target.value;
     //console.log(fieldValue);
-
 }
-
 
 const submitSearch = document.getElementById('search-field-submit');
 submitSearch.addEventListener("submit", handleSubmit);
@@ -45,12 +79,6 @@ function handleSubmit(e){
     e.preventDefault()
     getRecipes(fieldValue);
 }
-
-
-
-
-
-
 
 
 
